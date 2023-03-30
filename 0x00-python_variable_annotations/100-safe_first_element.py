6310fdeec4e0c96399a80536535c2be131488fb6
AbdiAbader <abdiprocode@gmail.com>
2023-03-30 22:55:35 +0300

    Advance task

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""advance task 1"""
+import typing
+
+
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> typing.Union[typing.Any, None]:
+    """ safe first element"""
+    if lst:
+        return lst[0]
+    else:
+        return None
+
