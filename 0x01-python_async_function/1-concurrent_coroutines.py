8cb509352bbe4deb663b43d839fc7543bf25980e
AbdiAbader <abdiprocode@gmail.com>
2023-04-03 14:40:15 +0300

    updated task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,10 +3,14 @@
 import asyncio
 import random
 from typing import List
-
+import heapq
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ list of all delays """
-    return [await wait_random(max_delay) for i in range(n)]
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    delays = []
+    for i in range(n):
+        delay = await wait_random(max_delay)
+        heapq.heappush(delays, delay)
+    return [heapq.heappop(delays) for _ in range(n)]
