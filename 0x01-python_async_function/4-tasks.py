ee4b5f1a240c4d060d0793b60020615ba0b2ec34
AbdiAbader <abdiprocode@gmail.com>
2023-04-03 15:02:37 +0300

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,9 +4,9 @@
 import asyncio
 import random
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
+def task_wait_random(n: int, max_delay: int) -> asyncio.Task:
     """ Returns an asyncio.Task """
-    return asyncio.create_task(wait_random(max_delay))
+    return asyncio.create_task(wait_n(n, max_delay))
