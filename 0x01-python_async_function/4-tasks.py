1a011eee800511c974e161dbd693c721b25f588f
AbdiAbader <abdiprocode@gmail.com>
2023-04-03 15:06:29 +0300

    fix pep8

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,6 +6,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     tasks = [task_wait_random(max_delay) for i in range(n)]
     delays = await asyncio.gather(*tasks)
