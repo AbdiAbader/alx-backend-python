f0b9cfccd063a437f6299ce79a171151e813325d
AbdiAbader <abdiprocode@gmail.com>
2023-04-24 17:22:06 +0300

    update pep8

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,7 +12,6 @@ from unittest.mock import PropertyMock
 class TestGithubOrgClient(unittest.TestCase):
     """Test Github Class"""
 
-
     @parameterized.expand([
         ('google',),
         ('abc',),
@@ -24,26 +23,24 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         client.org()
         mock_get.assert_called_once_with(url)
-    
+
     @patch.object(GithubOrgClient, "org", new_callable=PropertyMock)
     @patch('client.get_json')
     def test_public_repos_url(self, mock_org):
-        mock_org.return_value = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
+        url = "https://api.github.com/orgs/test_org/repos"
+        mock_org.return_value = {"repos_url": url}
         client = GithubOrgClient("test_org")
-        self.assertEqual(client._public_repos_url, "https://api.github.com/orgs/test_org/repos")
+        self.assertEqual(client._public_repos_url, url)
 
     @patch('client.get_json')
     def test_public_repos(self, mock_json):
         """Test public repos"""
+        url = "https://api.github.com/orgs/test/repos"
         mock_json.return_value = [{"name": "ALx"}, {"name": "TheRoom"}]
-        client = GithubOrgClient("test_org")
         with patch('client.GithubOrgClient._public_repos_url',
-               new_callable=PropertyMock,
-               return_value="https://api.github.com/orgs/test/repos") as mock_public:
-             self.assertEqual(GithubOrgClient('test').public_repos(),
-                              [i["name"] for i in [{"name": "ALx"}, {"name": "TheRoom"}]])
-             mock_public.assert_called_once()
-             mock_json.assert_called_once_with("https://api.github.com/orgs/test/repos")
-        
-        
-        
+                   new_callable=PropertyMock, return_value=url) as mock_public:
+            self.assertEqual(GithubOrgClient('test').public_repos(),
+                             [i["name"] for i in [{"name": "ALx"},
+                                                  {"name": "TheRoom"}]])
+            mock_public.assert_called_once()
+            mock_json.assert_called_once_with(url)
