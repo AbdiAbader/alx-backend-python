775b1448fd253879dcb25d42a1c638065e338670
AbdiAbader <abdiprocode@gmail.com>
2023-04-24 14:36:40 +0300

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -28,3 +28,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, expected_key):
         """Test access_nested_map exception"""
         self.assertRaisesRegex(KeyError, expected_key, access_nested_map, nested_map, path)
+    
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+
+
+    def test_get_json(self, test_url, test_payload):
+        """Test get_json"""
+        self.assertEqual(get_json(test_url), test_payload)
