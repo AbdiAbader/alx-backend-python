28a4f247edb1eff27ee7ea2ad04f27e5d489eefa
AbdiAbader <abdiprocode@gmail.com>
2023-04-24 16:11:51 +0300

    test client

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Test for Client"""
+
+import unittest
+from unittest import TestCase, mock
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test Github Class"""
+
+    @parameterized.expand([
+        ('google',),
+        ('abc',)
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get):
+        """Test Org"""
+        url = 'https://api.github.com/orgs/{org_name}'
+        client = GithubOrgClient(org_name)
+        client.org()
+        mock_get.assert_called_once_with(url)
